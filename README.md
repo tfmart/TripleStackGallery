a256eed9e2f78c88e366d874d3f49018714b08d9
Tomás Martins <tomas.marti@me.com>
2021-12-23 02:35:04 -0300

    Initial working version with colors

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# ThreeStripesGallery
+# TripleStackGallery
 
 A description of this package.
--- Sources/ThreeStripesGallery/ThreeStripesGallery.swift
+++
@@ -1,6 +0,0 @@
-public struct ThreeStripesGallery {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
