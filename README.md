04ad81f942e3f32581dd7883d723b95cf93175be
Tomás Martins <tomas.marti@me.com>
2021-12-23 01:00:51 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ThreeStripesGallery
+
+A description of this package.
---
+++ Sources/ThreeStripesGallery/ThreeStripesGallery.swift
@@ -0,0 +1,6 @@
+public struct ThreeStripesGallery {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
