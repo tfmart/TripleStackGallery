4bf68f1d635f7c6ad4fbf3c6d378a80ce6bf67b5
Tomás Martins <tomas.marti@me.com>
2021-12-24 02:28:46 -0300

    Index property

--- Sources/TripleStackGallery/TripleStackGallery.swift
+++ Sources/TripleStackGallery/TripleStackGallery.swift
@@ -12,13 +12,12 @@ struct TripleStackGallery: View {
     @State var currentOffset: CGSize = .init(width: -20, height: -20)
     @State var nextOffset: CGSize = .zero
     @State var lastOffset: CGSize = .init(width: 20, height: 20)
+
     // MARK: - Opacities
     @State var currentOpacity: Double = 1.0
+
     // MARK: Displayed views
-    @State var current: Color = .clear
-    @State var next: Color = .clear
-    @State var last: Color = .clear
-    @State var nextPage: Color = .clear
+    @State var index: Int = 0
     
     var colors: [Color]
     
@@ -30,70 +29,83 @@ struct TripleStackGallery: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                nextPage
-                    .aspectRatio(1.0, contentMode: .fill)
-                    .offset(.init(width: 20, height: 20))
-                last
-                    .aspectRatio(1.0, contentMode: .fill)
-                    .offset(lastOffset)
-                next
-                    .aspectRatio(1.0, contentMode: .fill)
-                    .offset(nextOffset)
-                current
-                    .aspectRatio(1.0, contentMode: .fill)
-                    .offset(currentOffset)
-                    .opacity(currentOpacity)
-                    .onAppear(perform: {
-                        initalSetup()
-                    })
-                    .gesture(DragGesture()
-                                .onChanged({ dragValue in
-                        if isValidGesture(width: dragValue.translation.width) {
-                            currentOffset = .init(width: dragValue.translation.width, height: -20)
-                        }
-                    })
-                                .onEnded({ endValue in
-                        if shouldProgress(endValue.translation.width) {
-                            withAnimation(.easeInOut(duration: duration)) {
-                                self.currentOpacity = 0.0
-                                currentOffset = .init(width: -20, height: -20)
-                                nextOffset = .init(width: -20, height: -20)
-                                lastOffset = .zero
+            if let current = currentColor {
+                ZStack {
+                    color(byAdding: 3)
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .offset(.init(width: 20, height: 20))
+                    color(byAdding: 2)
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .offset(lastOffset)
+                    color(byAdding: 1)
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .offset(nextOffset)
+                    current
+                        .aspectRatio(1.0, contentMode: .fill)
+                        .offset(currentOffset)
+                        .opacity(currentOpacity)
+                        .gesture(DragGesture()
+                                    .onChanged({ dragValue in
+                            if isValidGesture(width: dragValue.translation.width) {
+                                currentOffset = .init(width: dragValue.translation.width, height: -20)
                             }
-                            DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                                updateColors()
-                                currentOpacity = 1.0
-                                currentOffset = .init(width: -20, height: -20)
-                                nextOffset = .zero
-                                lastOffset = .init(width: 20, height: 20)
+                        })
+                                    .onEnded({ endValue in
+                            if shouldProgress(endValue.translation.width) {
+                                withAnimation(.easeInOut(duration: duration)) {
+                                    self.currentOpacity = 0.0
+                                    currentOffset = .init(width: -20, height: -20)
+                                    nextOffset = .init(width: -20, height: -20)
+                                    lastOffset = .zero
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                                    increaseIndex()
+                                    currentOpacity = 1.0
+                                    currentOffset = .init(width: -20, height: -20)
+                                    nextOffset = .zero
+                                    lastOffset = .init(width: 20, height: 20)
+                                }
+                            } else {
+                                withAnimation(.easeInOut) {
+                                    currentOffset = .init(width: -20, height: -20)
+                                }
                             }
-                        } else {
-                            withAnimation(.easeInOut) {
-                                currentOffset = .init(width: -20, height: -20)
-                            }
-                        }
-                    }))
-            }.padding(20)
+                        }))
+                }.padding(20)
+            } else {
+                Text("No data to display")
+            }
         }
     }
     
-    func initalSetup() {
-        self.current = self.colors[0]
-        self.next = nextColor(indexColor: current)
-        self.last = nextColor(indexColor: next)
-        self.nextPage = nextColor(indexColor: last)
-    }
-    
-    func nextColor(indexColor: Color) -> Color {
-        guard let index = colors.firstIndex(of: indexColor) else {
-            return .clear
-        }
-        if index + 1 == colors.count {
-            return colors[0]
+    func increaseIndex() {
+        guard colors.count > 0 else { return }
+        if index == colors.count - 1 {
+            index = 0
         } else {
-            return colors[index+1]
+            index += 1
         }
+    }
+    
+    var currentColor: Color? {
+        guard colors.indices.contains(index) else { return nil }
+        return colors[index]
+    }
+    
+    func color(byAdding index: Int) -> Color {
+        let colorIndex = self.index + index
+        guard colors.indices.contains(colorIndex) else {
+            return colors[actual(index: colorIndex)]
+        }
+        return colors[colorIndex]
+    }
+    
+    func actual(index overflowIndex: Int) -> Int {
+        let actualIndex = overflowIndex - colors.count
+        guard colors.indices.contains(actualIndex) else {
+            return actual(index: actualIndex)
+        }
+        return actualIndex
     }
     
     func isValidGesture(width: CGFloat) -> Bool {
@@ -102,17 +114,6 @@ struct TripleStackGallery: View {
     
     func shouldProgress(_ width: CGFloat) -> Bool {
         return width > 60 || width < -60
-    }
-    
-    func updateColors() {
-        current = next
-        next = last
-        last = nextPage
-        if nextPage == colors.last {
-            nextPage = colors[0]
-        } else {
-            nextPage = colors[colors.firstIndex(of: nextPage)! + 1]
-        }
     }
 }
 
